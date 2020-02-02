8ced887f9d93285ba2b5f4ca63d3ace25e701a99
AntonMitsura <mitsuraanton@mail.ru>
2020-02-02 13:18:09 +0300

    1

--- src/main/java/com/ifmo/diploma/mitsura/anton/DiplomaMitsuraAnton/repository/GroupedOrdersRepository.java
+++ src/main/java/com/ifmo/diploma/mitsura/anton/DiplomaMitsuraAnton/repository/GroupedOrdersRepository.java
@@ -3,8 +3,6 @@ package com.ifmo.diploma.mitsura.anton.DiplomaMitsuraAnton.repository;
 import com.ifmo.diploma.mitsura.anton.DiplomaMitsuraAnton.entity.GroupedOrders;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
-
 public interface GroupedOrdersRepository extends CrudRepository<GroupedOrders, Integer> {
     Iterable<GroupedOrders> findByClientNameAndClientPasswordOrderByGroupIdDesc(String clientName, String clientPassword);
     GroupedOrders findByGroupIdAndClientNameAndClientPassword(int groupId, String clientName, String clientPassword);
